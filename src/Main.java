594df5d287e9fa4df1af13874a101a68ac08482d
ams32ru <ams32ru@mail.ru>
2022-09-29 14:05:07 +0300

    Исправился, запутался с функциями и где что писать, сейчас немного прояснилось

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,61 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Main {
-    // уважаемый проверяющий, мне кажется что я написал плохо читаемы код,
-    // вероятно потуму что еще не разобрался с зонами видимости и вообще с методами,
-    // но все почему то работает
     public static void main(String[] args) {
-        generateRandomArray();
-        task4();
+        int [] expenses = generateRandomArray();
+        System.out.println(Arrays.toString(expenses));
+        System.out.println("Задание 1");
+        int sum = 0;
+        for (int exe :expenses) {
+            sum = sum +exe;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+        System.out.println("Задание 2");
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int exp:expenses) {
+            if (exp > max){
+                max = exp;
+            }
+            if (exp < min){
+                min = exp;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min +
+                "рублей. ");
+        System.out.println("Максимальная сумма трат за день составила " + max +
+                "рублей. ");
+
+        System.out.println("Задание 3");
+        float averageExpenses = (float) sum / expenses.length;
+        System.out.println ("Cредняя сумма трат за месяц составила " + averageExpenses + " рублей ");
+        System.out.println("Задание 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length -1; i >=0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
+
+
+
+
+
+
+
     }
 
-    int[] arr = generateRandomArray();
+
+
 
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
-
         }
-        System.out.println("Задание 1");
-        int moneyMont = 0;
-        for (int i = 0; i < arr.length; i++) {
-            moneyMont = moneyMont + arr[i];
-        }
-        System.out.println("Сумма трат за месяц составила " + moneyMont + " рублей.");
-        System.out.println("Задание 2");
-        //как я понял можно указывать первый индекс массива для перебора, или просто число которое
-        // будет больше или меньше нужного нам
-        int minMoney = arr[0];
-        int maxMoney = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr.length > maxMoney) {
-                maxMoney = arr[i];
-            }
-        }
-        System.out.println("Максимальная сумма трат за день составила " + maxMoney + " рублей.");
-        for (int i = 0; i < arr.length; i++) {
-            if (arr.length < minMoney) {
-                minMoney = arr[i];
-            }
-        }
-        System.out.println("Минимальная сумма трат за день составила " + minMoney + " рублей.");
-        System.out.println("Задание 3");
-        float summ = 0f;
-        for (int i = 0; i < arr.length; i++) {
-            summ = summ + arr[i];
-        }
-        System.out.println("Средняя сумма трат за месяц составила " + summ / arr.length + " рублей.");
-
-
         return arr;
     }
-
-    public static void task4() {
-        System.out.println("Задание 4");
-        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-        }
-
-
-    }
-
-
 }
 
